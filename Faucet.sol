eeae0c6e8e6eaee507f9674c08816c402ead1ac4
Sergey Korolev <korolevsv@gmail.com>
2019-02-28 01:26:50 +0300

    The initial commit

---
+++ Faucet.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.19;
+// Our first contract is a faucet!
+contract Faucet {
+
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    // Accept any incoming amount
+    function () public payable {}
+
+}
